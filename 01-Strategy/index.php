2f06fe7087dfe8c5a6bccaac78c1e05fe9f42de6
Hugo Gomes <hdsg0807@gmail.com>
2020-01-28 22:59:27 -0300

    First Commit

---
+++ 01-Strategy/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require('./Orcamento.php');
+require('./Imposto.php');
+require('./CalculadoraDeImpostos.php');
+require('./ICMS.php');
+require('./ISS.php');
+require('./KCV.php');
+
+$reforma = new Orcamento(500);
+
+$calculadora = new CalculadoraDeImpostos();
+
+echo $calculadora->calcula($reforma, new ICMS());
+
+echo "<br/>";
+
+echo $calculadora->calcula($reforma, new ISS());
+
+echo "<br/>";
+
+echo $calculadora->calcula($reforma, new KCV());
+
+?>
